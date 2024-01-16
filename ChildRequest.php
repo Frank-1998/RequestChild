339fbdf26df183fb5d4362287f881f0442b97921
Frank <ziang6@ualberta.ca>
2024-01-16 13:01:30 -0700

    finish implementing the method to retrieve std dev, update readme for references.

--- ChildRequest.php
+++ ChildRequest.php
@@ -51,6 +51,32 @@ class ChildRequest extends Request
         return $meanTimeArray;
     }
 
+    /**
+     * Retrieve the standard deviation of each URI
+     * 
+     * @param 
+     * @return array The array that contains a standard deviation of each URI
+     */
+    public function retrieveStdDev(): array
+    {
+        $meanArray = $this->retrieveMeanResTime();
+        $stdDevArray = [];
+        $keys = array_keys($this->responseTimes);
+        foreach($keys as $key){
+            $sum = 0.0; // for summation
+            $num_data = count($this->responseTimes[$key]); // n of each URI
+            $mean_i = $meanArray[$key][0]; // mean of each URI
+            var_dump($mean_i);
+
+            // to calculate the sum of square of the (x - x_mean)
+            foreach($this->responseTimes[$key] as $time){
+                $sum += pow($time - $mean_i,2);
+            }
+            $stdDev = sqrt($sum/($num_data-1)); // calculate std dev
+            $stdDevArray[$key][] = $stdDev;
+        }
+        return $stdDevArray;
+    }
 
     /**
      * method to record one response time for a specific uri
--- README.md
+++ README.md
@@ -4,3 +4,7 @@ GoodMorning.com candidate exercise
 
 ## UML Class Diagram
 ![uml diagram](uml_diagram.png)
+
+
+## References
+[time effcicency compare between array_map and forech loop](https://stackoverflow.com/questions/25481714/php-is-array-map-faster-than-foreach)
