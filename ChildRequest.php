9fd03087e7ddd5a06bf46968ccdc3929e78e13d5
Frank <ziang6@ualberta.ca>
2024-01-19 22:06:45 -0700

    Finish Clean up
    Tried best to improve time efficiency given current design. Added More comments to make code more clear.

--- ChildRequest.php
+++ ChildRequest.php
@@ -69,7 +69,7 @@ class ChildRequest extends Request
      */
     public function retrieveHistogram(): array
     {
-        $histogramData = $this->retrieveHistogramData();
+        $histogramData = $this->generateHistogramData();
         // not enough uri to draw the histogram
         if (count($histogramData) == 0){
             return [];
@@ -87,14 +87,16 @@ class ChildRequest extends Request
         $keys = array_keys($this->responseTimes);
         $imgArray = [];
         foreach($keys as $key){
-            // create image and allocate color, draw border and fill color
-            $img = imagecreate($imgWidth, $imgHeight);
+            // create image
+            $img = imagecreate($imgWidth, $imgHeight); 
+            // define colors
             $barColor=imagecolorallocate($img,0,64,128);
             $backgroundColor=imagecolorallocate($img,240,240,255);
             $borderColor=imagecolorallocate($img,200,200,200);
             $lineColor=imagecolorallocate($img,220,220,220);
-            imagefilledrectangle($img,1,1,$imgWidth-2,$imgHeight-2,$borderColor);
-            imagefilledrectangle($img,$margins,$margins,$imgWidth-1-$margins,$imgHeight-1-$margins,$backgroundColor);
+            // draw border and fill background color
+            imagefilledrectangle($img,1,1,$imgWidth-2,$imgHeight-2,$borderColor); // draw border
+            imagefilledrectangle($img,$margins,$margins,$imgWidth-1-$margins,$imgHeight-1-$margins,$backgroundColor); // fill background color
             
             $data = $histogramData[$key]; // data for each URI
             $numBars = count($data); // number of bars
@@ -121,11 +123,10 @@ class ChildRequest extends Request
                 $x2 = $x1 + $barWidth;
                 $y1 = $margins + $graphHeight - intval($binData*$ratio);
                 $y2 = $imgHeight - $margins;
-                imagestring($img, 0, $x1+3, $y1-10, $binData, $barColor);
-                imagestring($img,0,$x1+3,$imgHeight-15,$xLabel,$barColor);
-                imagefilledrectangle($img,$x1,$y1,$x2,$y2,$barColor);
+                imagestring($img, 0, $x1+6, $y1-10, $binData, $barColor); // draw labels on the bins
+                imagestring($img,0,$x1-20,$imgHeight-15,$xLabel,$barColor); // draw x-aixs lables
+                imagefilledrectangle($img,$x1,$y1,$x2,$y2,$barColor); // color the bins
             }
-            // $imgArray[$key] = $img;
             $imgArray[$key] = $img;
             // reset image to default state
             imagedestroy($img);
@@ -142,34 +143,30 @@ class ChildRequest extends Request
      * @param
      * @return array The histogram data(category and number of each category) of each URI 
      */
-    private function retrieveHistogramData(): array
+    private function generateHistogramData(): array
     {
-        
-        $normalizedData = $this->normalizeData();
+        $normalizedDataArrays = $this->normalizeData();
         // if there is no normalized data, can't generate histogram data.
-        if (count($normalizedData) == 0){
+        if (empty($normalizedDataArrays)){
             return [];
         }
         $histogramData = [];
-        $keys = array_keys($this->responseTimes);
-        foreach ($keys as $key){
-            $data = $normalizedData[$key];
-            $numOfUnique = count(array_unique($data));
-            if ($numOfUnique <= $this->maxNumBins){
-                $data = array_map('strval', $data);
-                // there is less data than maximum bin number, # of bins < maximum bins number
-                $histogramData[$key] = array_count_values($data);
-            }
-            else{ // divide the data into ranges(bins)
-                $min_value = min($data);
-                $max_value = max($data);
-                $range = $max_value - $min_value;
-                $binRange = $range/$this->maxNumBins;
-                for ($i = 1; $i<=$this->maxNumBins; $i++){ // using max and min value to divide bins evenly
-                    $start = $min_value;
-                    $finish = $min_value + $binRange;
-                    $histogramData[$key][strval(round($start,2)."~".round($finish,2))] = $this->countInRange($start, $finish, $data);
-                    $min_value += ($binRange+0.0000000000001);
+        foreach ($this->responseTimes as $key => $time){
+            $normDataArray = $normalizedDataArrays[$key]; // get normalized data for this URI
+            // # of unique data <= max bins, count each data's frequency and use data itself as label for x-aixs
+            if(count(array_unique($normDataArray)) <= $this->maxNumBins){
+                $normDataArray= array_map('strval', $normDataArray);
+                $histogramData[$key] = array_count_values($normDataArray);
+            } else { # of unique data > max bins, group data into evenly divided ranges, and count frequency
+                $minValue = min($normDataArray);
+                $maxValue = max($normDataArray);
+                $binRange = ($maxValue-$minValue)/$this->maxNumBins;
+
+                for($i = 1; $i <= $this->maxNumBins; $i++){
+                    $rangeStart = $minValue;
+                    $rangeEnd = $minValue + $binRange;
+                    $histogramData[$key][strval(round($rangeStart,2))."~".strval(round($rangeEnd,2))] = $this->countInRange($rangeStart, $rangeEnd, $normDataArray);
+                    $minValue += ($binRange+0.0000000000001); // increase minValue to prevent range overlap
                 }
             }
         }
@@ -209,22 +206,18 @@ class ChildRequest extends Request
             return [];
         }
         $normalizedData = [];
-        $keys = array_keys($this->responseTimes); 
-        $means = $this->retrieveMeanResTime();
-        $stdDevs = $this->retrieveStdDev();
-
-        foreach($keys as $key){
-            $data = $this->responseTimes[$key];
-            // if there is only 1 data, can't normalize it, continue to next URI
-            if (count($data) < 2){
-                $normalizedData[$key][] = $data[0];
+        $means = $this->retrieveMeanResTime(); // get means
+        $stdDevs = $this->retrieveStdDev(); // get std devs
+        foreach($this->responseTimes as $key=>$time){
+            if (count($time) < 2){ // not enough data to normalize, return original data
+                $normalizedData[$key][] = $time[0];
                 continue;
             }
+            // calculate normalized data
             $mean_i = $means[$key];
             $std_i = $stdDevs[$key];
-            foreach($data as $time){
-                $normalizedTime = ($time - $mean_i)/$std_i;
-                $normalizedData[$key][] = $normalizedTime;
+            foreach($time as $data){
+                $normalizedData[$key][]=($data - $mean_i)/$std_i;
             }
         }
         return $normalizedData;
@@ -244,6 +237,7 @@ class ChildRequest extends Request
         if (count($this->responseTimes) == 0){
             return [];
         }
+        // calculate means
         foreach($this->responseTimes as $key=>$times){
             $meanTimeArray[$key] = array_sum($times)/count($times);
         }
@@ -272,6 +266,7 @@ class ChildRequest extends Request
                 $stdDevArray[$key] = 0.0;
                 continue;
             }
+            // calculate standard deviations
             $mean_i = $meanArray[$key];
             $sum = 0.0;
             foreach($times as $time){
@@ -297,25 +292,6 @@ class ChildRequest extends Request
         }
         $this->responseTimes[$uri][] = $responseTime;
     }
-
-
-    // -------------------------------------- testing purpose ---------------------------------
-    public function showResTimeArray() : void {
-        var_dump($this->responseTimes); 
-    }    
 }
 
-
-
-$astiri = new ChildRequest(5);
-$astiri->childProcess('tt');
-$astiri->childProcess('tt');
-$astiri->childProcess('tt');
-$astiri->childProcess('gg');
-echo "time array\n";
-$astiri->showResTimeArray();
-echo "\nmean\n";
-var_dump($astiri->retrieveMeanResTime());
-echo "\nStd Dev\n";
-var_dump($astiri->retrieveStdDev());
 ?>
